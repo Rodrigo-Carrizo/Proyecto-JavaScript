765e842d17e40dfb1dbf71381285a0d4eacae3d1
Rodrigo Carrizo <rodrigocarrizo.210@gmail.com>
2022-11-28 22:06:53 -0300

    Pre-Entrega-3

--- app.js
+++ app.js
@@ -19,31 +19,35 @@ class Movimientos {
       this.tipoDeMovimiento = tipoDeMovimiento;
       this.fechaDeMovimiento = fechaDeMovimiento;
     }
-  }
+}
   
-function iniciarSesion(){
-    const nombre = prompt("Nombre de Usuario");
-    const tarjeta = prompt("Numero de tarjeta");
-  
+function iniciarSesion(nombre, tarjeta){
     usuarioLogIn = usuarios.find((usuario) => usuario.nombre === nombre && usuario.tarjeta === tarjeta );
-  
+    
     if(usuarioLogIn) {
-      menuDeOperaciones();
+      localStorage.setItem("usuario", JSON.stringify(usuarioLogIn));
+  
+      let mensajeBienvenida = document.getElementById("bienvenida");
+      let nombreUsuario = document.getElementById("nombreUsuario");
+      mensajeBienvenida.className = "";
+      nombreUsuario.innerText = nombre;
+  
+      formularioIniciarSesion.className = "hidden";
+      formularioRegistrarse.className = "hidden";
     } else {
       alert("No se encuentra este usuario.");
     }
 }
   
-function registrarse() {
-    const nombre = prompt("Nombre de Usuario");
-    const tarjeta = prompt("Numero de tarjeta");
-    const dinero = parseFloat(prompt("Cuanto dinero tiene?"));
-  
-    usuarios.push(new Usuario(nombre, tarjeta, dinero));
+function registrarse(nombre, tarjeta, dinero) {
+    const newUser = new Usuario(nombre, tarjeta, dinero);
+    usuarios.push(newUser);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    iniciarSesion(nombre, tarjeta);
 }
-
-function consultarDinero() {
-    alert("Usuario, su saldo actual es $${usuarioLogIn.dineroEnElBanco}");
+  
+  function consultarDinero() {
+    alert(`Usuario, su saldo actual es $${usuarioLogIn.dineroEnElBanco}`);
 }
   
 function validarSaldo(dineroARetirar) {
@@ -72,7 +76,7 @@ const depositarDinero = () => {
     if(dineroADepositar > 0) {
       usuarioLogIn.dineroEnElBanco += dineroADepositar;
       const fecha = new Date();
-      usuarioLogIn.agregarMovimiento(new Movimientos(dineroADepositar, "Deposito", fecha));
+      usuarioLogIn.agregarMovimiento(new Movimientos(dineroADepositar, 'Deposito', fecha));
       alert(`Su nuevo saldo es de ${usuarioLogIn.dineroEnElBanco}`)
     } else {
       alert("El valor es incorrecto.")
@@ -85,7 +89,7 @@ const transferirDinero = () => {
     if(resultado) {
       const fecha = new Date();
       usuarioLogIn.agregarMovimiento(new Movimientos(dineroATrasferir, "Transferencia", fecha));
-      alert(`La transferencia se realizo con exito, su nuevo saldo es de ${usuarioLogIn.dineroneroDelUsuario}`);
+      alert(`La transferencia se realizo con exito, su nuevo saldo es de ${diusuarioLogIn.dineroneroDelUsuario}`);
     }
 }
   
@@ -114,38 +118,66 @@ function seleccionarOperacionDelMenu(operacion) {
         break;
       case "6":
         alert("Nos vemos.");
+        localStorage.removeItem("usuario");
         break;
       default:
-        alert("No existe la opcion ingresada, intente de nuevo.");
         break;
     }
 }
   
-function menuDeOperaciones() {
-  do {
-    opcionMenu = prompt("Ingresa la operaciona a realizar. \n1. Consultar saldo. \n2. Retirar dinero. \n3. Depositar dinero. \n4. Transferir dinero. \n5. Ver movimientos. \n5. Salir.")
-    seleccionarOperacionDelMenu(opcionMenu);
-  } while( opcionMenu !== "6" );
-}
+let formularioIniciarSesion = document.getElementById("iniciarSesion");
+let formularioRegistrarse = document.getElementById("registrarse");
+let menuDeOperaciones = document.getElementById("operaciones");
   
-let opcionInicio = 0;
-let usuarios = [];
-let usuarioLogIn;
-let opcionMenu = 0;
-  do {
-    opcionInicio = prompt("Bienvenido! que opcion desea realizar?\n1.Iniciar sesion.\n2.Registrarse.\n3.Salir");
-    switch(opcionInicio) {
-      case "1":
-        iniciarSesion();
-        break;
-      case "2":
-        registrarse();
-        break;
-      case "3":
-        alert("Nos vemos.");
-        break;
-      default:
-        alert("Opcion incorrecta.");
-        break;
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+let usuarioLogIn = JSON.parse(localStorage.getItem("usuario"));
+  
+  
+if(usuarioLogIn) {
+    let mensajeBienvenida = document.getElementById("bienvenido");
+    let nombreUsuario = document.getElementById("nombreUsuario");
+  
+    mensajeBienvenida.className = "";
+    nombreUsuario.innerText = usuarioLogIn.nombre;
+    menuDeOperaciones.className = "row gx-5 gy-2";
+    formularioIniciarSesion.className = "hidden";
+    formularioRegistrarse.className = "hidden";
+  
+    let botones = document.querySelectorAll(".btn-secondary");
+    for (const boton of botones) {
+      boton.addEventListener("click", (e) => {
+        let dataId = e.target.getAttribute("data-id");
+        seleccionarOperacionDelMenu(dataId);
+      });
     }
-  } while(opcionInicio != 3);
+} 
+  
+else {
+    formularioIniciarSesion.className = "";
+    formularioRegistrarse.className = "mt-2";
+  
+    formularioIniciarSesion.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let usuario = document.getElementById("usuario").value;
+      let numeroTarjeta = document.getElementById("numeroTarjeta").value;
+  
+      if(usuario != '' && numeroTarjeta != '') {
+        iniciarSesion(usuario, numeroTarjeta);
+      } else {
+        alert("Todos los datos son obligatorios");
+      }
+    });
+  
+    formularioRegistrarse.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let usuario = document.getElementById("usuarioRegistrado").value;
+      let numeroTarjeta = document.getElementById("numeroTarjetaRegistrado").value;
+      let dinero = document.getElementById("dinero").value;
+  
+      if(usuario != "" && numeroTarjeta != "" && dinero != "") {
+        registrarse(usuario, numeroTarjeta, parseFloat(dinero));
+      } else {
+        alert("Todos los datos son obligatorios");
+      }
+    });
+  }
